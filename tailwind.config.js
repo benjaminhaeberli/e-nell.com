dacbce89dabd888c537a5a5c59bcba26a7dbaa77
Benjamin Haeberli <admin@benjaminhaeberli.ch>
2023-07-10 11:57:19 +0200

    ✨ block e, block f, block g, block n, better buttons (internal/external)

--- tailwind.config.js
+++ tailwind.config.js
@@ -34,6 +34,7 @@ module.exports = {
         sm: "640px",
         md: "768px",
         lg: "1024px",
+        bs: "1140px",
         xl: "1280px",
         xxl: "1440px",
         "2xl": "1536px",
